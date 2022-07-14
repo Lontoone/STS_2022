0ab77aab9d13a1a33ff0526ef881898026c6dd90
oi <oi@daddylove.com>
2022-07-14 16:41:21 +0800

    放假？

--- Assets/syska/Scripts/PlayerHUD.cs
+++ Assets/syska/Scripts/PlayerHUD.cs
@@ -7,8 +7,10 @@ public class PlayerHUD : MonoBehaviour
 {
     public static PlayerHUD instance;
     public Text Health;
+    public Text Codes;
 
     private void Awake() { instance = this; }
 
-    public static void UpdateLifes() { instance.Health.text = "Lifes: " + Player.Lifes.ToString(); }
+    public static void UpdateLifes() { instance.Health.text = $"Lifes: {Player.Lifes}/3"; }
+    public static void UpdateCodes() { instance.Codes.text = "Codes: "; }
 }
