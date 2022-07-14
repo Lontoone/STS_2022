03eb544c4f18020894ba8ef4e762c47d54ad94aa
oi <oi@daddylove.com>
2022-07-14 14:14:51 +0800

    Basic HUD

--- Assets/syska/Scripts/Player.cs
+++ Assets/syska/Scripts/Player.cs
@@ -13,11 +13,15 @@ public class Player : MonoBehaviour
     private float YLock;
     private bool mLock;
 
+    [Header("Player Movement")]
     public float movespeedmultipy = 1;
     private Vector3 finalvelocity;
 
     public float HoverHeight = 0.2f;
     private RaycastHit hhRayHit;
+
+    [Header("Player States")]
+    public static int Lifes = 3;
 
     private void Awake()
     {
@@ -28,10 +32,13 @@ public class Player : MonoBehaviour
         mLock = true;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        PlayerHUD.UpdateLifes();
     }
 
     private void Update()
     {
+        #region Cursor Lock/Release
         if (Input.GetMouseButtonDown(0))
         {
             mLock = true;
@@ -44,17 +51,24 @@ public class Player : MonoBehaviour
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
+        #endregion
 
+        #region Player Camera Up/Down
         YLock += Input.GetAxisRaw("Mouse Y") * MouseSens;
         YLock = Mathf.Clamp(YLock, -89, 89);
         c.transform.localEulerAngles = Vector3.left * YLock;
+        #endregion
 
-        transform.eulerAngles += Vector3.up * Input.GetAxisRaw("Mouse X") * MouseSens;
+        #region Player Rotate and Move
+        transform.eulerAngles += Input.GetAxisRaw("Mouse X") * MouseSens * Vector3.up;
 
         finalvelocity = Vector3.zero;
         finalvelocity += transform.right * Input.GetAxisRaw("Horizontal");
         finalvelocity += transform.forward * Input.GetAxisRaw("Vertical");
         finalvelocity *= movespeedmultipy;
+        #endregion
+
+        if (Input.GetKeyDown(KeyCode.K)) Damage();
     }
 
     private void FixedUpdate()
@@ -77,5 +91,11 @@ public class Player : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawLine(transform.position + Vector3.up, hhRayHit.point);
+    }
+
+    public static void Damage()
+    {
+        Lifes--;
+        PlayerHUD.UpdateLifes();
     }
 }
