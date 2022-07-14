2352d92880fd6b42341a3e5a30334a116be34e87
oi <oi@daddylove.com>
2022-07-14 13:36:17 +0800

    fix player camera axis, improve player collider, improve player camera movement

--- Assets/syska/Scripts/Player.cs
+++ Assets/syska/Scripts/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
 
     public float MouseSens = 3.1f;
     private float YLock;
+    private bool mLock;
 
     public float movespeedmultipy = 1;
     private Vector3 finalvelocity;
@@ -23,34 +24,58 @@ public class Player : MonoBehaviour
         instance = this;
         r = GetComponent<Rigidbody>();
         c = Camera.main;
+
+        mLock = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
     {
-        c.transform.localEulerAngles += Vector3.left * Input.GetAxisRaw("Mouse Y") * MouseSens;
-        YLock += Input.GetAxis("Mouse X") * MouseSens;
+        if (Input.GetMouseButtonDown(0))
+        {
+            mLock = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            mLock = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        YLock += Input.GetAxisRaw("Mouse Y") * MouseSens;
         YLock = Mathf.Clamp(YLock, -89, 89);
-        transform.eulerAngles = Vector3.up * YLock;
+        c.transform.localEulerAngles = Vector3.left * YLock;
+
+        transform.eulerAngles += Vector3.up * Input.GetAxisRaw("Mouse X") * MouseSens;
 
         finalvelocity = Vector3.zero;
-        finalvelocity += transform.right * Input.GetAxis("Horizontal");
-        finalvelocity += transform.forward * Input.GetAxis("Vertical");
+        finalvelocity += transform.right * Input.GetAxisRaw("Horizontal");
+        finalvelocity += transform.forward * Input.GetAxisRaw("Vertical");
         finalvelocity *= movespeedmultipy;
-        if (Physics.Raycast(transform.position, transform.up * -1, out hhRayHit, HoverHeight + 0.1f))
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position+Vector3.up, transform.up * -1, out hhRayHit, HoverHeight + 1.1f))
         {
-            if (hhRayHit.distance < HoverHeight - 0.1f)
+            if (hhRayHit.distance < HoverHeight + 1)
             {
-                transform.position += Vector3.up * 0.1f;
+                transform.position += Vector3.up * 0.05f;
             }
         }
         else
         {
             finalvelocity += Vector3.down * 9.8f;
         }
+        r.velocity = finalvelocity;
     }
 
-    private void FixedUpdate()
+    private void OnDrawGizmos()
     {
-        r.velocity = finalvelocity;
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position + Vector3.up, hhRayHit.point);
     }
 }
