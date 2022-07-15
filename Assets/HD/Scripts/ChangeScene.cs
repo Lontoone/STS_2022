d8136dd24cfc2597a95947d26f28d2249e92cd18
Andy SHU <andy091045@gmail.com>
2022-07-15 22:23:19 +0800

    加入howtoplay 的 Scene

---
+++ Assets/HD/Scripts/ChangeScene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+// This script changes Scene, return to start and finish game 
+public class ChangeScene : MonoBehaviour
+{
+    //"Scene in build" number array
+    public int[] SceneNumber;
+
+    //i is the 'i'th one in SceneNumber array 
+    public void OnButtonClick(int i)
+    {
+        SceneManager.LoadScene(SceneNumber[i]);
+    }
+
+    public void Leave()
+    {
+         Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            //print H return to the Scene "Home" 
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown("escape"))
+        {
+            //print ESC to quit the game      
+            Application.Quit();
+        }
+    }
+}
